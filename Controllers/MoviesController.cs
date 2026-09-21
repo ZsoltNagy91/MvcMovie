97b323c4d45749211a26abbb3b01090772aa344d
ZsoltNagy91 <zsolt.j.nagy@gmail.com>
2026-09-21 18:51:12 +0200

    Add search

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,8 +1,9 @@
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using MvcMovie.Models;
 using MvcMovie.Data;
+using MvcMovie.Models;
 
 public class MoviesController : Controller
 {
@@ -12,12 +13,42 @@ public class MoviesController : Controller
     {
         _context = context;
     }
-
+    
     // GET: MOVIES
-    public async Task<IActionResult> Index()    
+   
+    public async Task<IActionResult> Index(string movieGenre, string searchString)
     {
-        return View(await _context.Movie.ToListAsync());
+        if (_context.Movie == null)
+        {
+            return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+        }
+
+        // Use LINQ to get list of genres.
+        IQueryable<string> genreQuery = from m in _context.Movie
+                                        orderby m.Genre
+                                        select m.Genre;
+        var movies = from m in _context.Movie
+                     select m;
+
+        if (!string.IsNullOrEmpty(searchString))
+        {
+            movies = movies.Where(s => s.Title!.ToUpper().Contains(searchString.ToUpper()));
+        }
+
+        if (!string.IsNullOrEmpty(movieGenre))
+        {
+            movies = movies.Where(x => x.Genre == movieGenre);
+        }
+
+        var movieGenreVM = new MovieGenreViewModel
+        {
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+            Movies = await movies.ToListAsync()
+        };
+
+        return View(movieGenreVM);
     }
+
 
     // GET: MOVIES/Details/5
     public async Task<IActionResult> Details(int? id)
--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75a4d332fa55bf6efcc1a3ca5f5ff733fb1a2742")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a664e44e50adbbcdd9bd8f9edcbb67f4cdeed6fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
